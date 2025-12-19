ca38c2b3c3bfac3d56a2b909a717007151048cf5
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2025-12-19 09:43:15 +0100

    feat(patricktree.me): align header items at start

--- apps/patricktree.me/src/components/header/Header.tsx
+++ apps/patricktree.me/src/components/header/Header.tsx
@@ -12,11 +12,15 @@ export const Header: React.FC<{ children: React.ReactNode }> = ({ children }) =>
 export const HeaderContent = styled.div`
   display: flex;
   gap: calc(4 * var(--spacing-base));
-  align-items: center;
+  align-items: flex-start;
   justify-content: space-between;
 
-  & > * {
+  & > *:nth-child(1) {
     padding-block-start: calc(1 * var(--spacing-base));
     padding-block-end: calc(2 * var(--spacing-base));
+  }
+  & > *:nth-child(2) {
+    padding-block-start: calc(1.5 * var(--spacing-base));
+    padding-block-end: calc(3 * var(--spacing-base));
   }
 `;
