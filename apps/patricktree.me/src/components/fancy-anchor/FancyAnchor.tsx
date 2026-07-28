8488e700e46603fa7e640be7fa340ab133a2de34
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2026-07-28 16:06:38 +0200

    cleanup

--- apps/patricktree.me/src/components/fancy-anchor/FancyAnchor.tsx
+++ apps/patricktree.me/src/components/fancy-anchor/FancyAnchor.tsx
@@ -14,15 +14,17 @@ export type FancyAnchorProps = AnchorProps & {};
  * The `FancyAnchor` allows to put an icon right at the beginning of the anchor.
  *
  * It applies some logic to get a nice line wrapping behaviour. First it does a rudimentary scan for
- * a line wrapping opportunity. Then: - If there is no wrap opportunity and the text is rather
- * short, it will be put with the icon in one "group" (with `display: inline-block`). The result is
- * that the anchor content does not line wrap; the whole anchor, together with the icon, will be put
- * into the next line if necessary. - If there is a wrap opportunity and the text part until that
- * wrap opportunity is rather short, that text part will be put into one "group" with the icon (so
- * that they wrap together into the next line if necessary). The rest of the text will be put into a
- * normal span. - Otherwise - if there is long text without a wrap opportunity, or the first text
- * part would be rather long - everything will just be put in a span with `word-break: break-all`,
- * meaning the anchor's content will wrap at every character.
+ * a line wrapping opportunity. Then:
+ *
+ * - If there is no wrap opportunity and the text is rather short, it will be put with the icon in one
+ *   "group" (with `display: inline-block`). The result is that the anchor content does not line
+ *   wrap; the whole anchor, together with the icon, will be put into the next line if necessary.
+ * - If there is a wrap opportunity and the text part until that wrap opportunity is rather short,
+ *   that text part will be put into one "group" with the icon (so that they wrap together into the
+ *   next line if necessary). The rest of the text will be put into a normal span.
+ * - Otherwise - if there is long text without a wrap opportunity, or the first text part would be
+ *   rather long - everything will just be put in a span with `word-break: break-all`, meaning the
+ *   anchor's content will wrap at every character.
  *
  * "rather short" and "rather long" was just determined by some manual testing :)
  */
